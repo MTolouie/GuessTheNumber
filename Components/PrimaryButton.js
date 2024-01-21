c358efa331d149bd9b11a56aab9e1feb57561c90
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2024-01-21 20:13:39 +0300

    added styles to te PrimaryButtons and textInput

--- Components/PrimaryButton.js
+++ Components/PrimaryButton.js
@@ -1,11 +1,42 @@
-import { View, Text,pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = (props) => {
+  const pressHandler = () => {};
+
   return (
-    <View>
-      <Text>{props.children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed ? [styles.buttonInnerContainer,styles.pressed] : styles.buttonInnerContainer
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#9c024f',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
